6272973941dbc0b0e629c6d4230768686cdf5056
Philipp Herkert <philipp.herkert@gmail.com>
2016-10-06 17:22:32 +0200

    First Commit

---
+++ testaid2/src/main/java/testgid2/testaid2/App.java
@@ -0,0 +1,13 @@
+package testgid2.testaid2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
