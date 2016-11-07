79a520a0b9faa622449e1bedf19cd45ec28e6fc4
Philipp Herkert <philipp.herkert@gmail.com>
2016-11-07 12:02:03 +0100

    Test Änderungen

--- testaid2/src/main/java/testgid2/testaid2/App.java
+++ testaid2/src/main/java/testgid2/testaid2/App.java
@@ -2,70 +2,88 @@ package testgid2.testaid2;
 
 /**
  * Hello world!
- * 
+ *
  */
-public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello Worlaöldskfjalksfjdasölkfjd!");
-		System.out.println(vigenere("ABC", new int[] { 2, 3, 4 }));
-		System.out.println(anzahl("aaaabbbaaeee",'a',2,1));
-		System.out.println(vigenere("DADPSGSHCIITQLRGLDYEKCIYAPORLSKHFOETSLWLQBDAUPSNYYRIRNRCEK",new int[]{-2,-15,-3,-7,-0}));
-		String c = "HLNFYJXPTGOFRTBXOFQHUMRFVJIEYMIZCEICENFKLMEUHIYTXTBPZEMLFRAAMMGIYVRNFMUFVCJKLOIYFGOJJMSIKJIZFIUUHLDOBOKDVVKFNLESJIHHNESTRPDLAPIMGIUUPPDLIFOHORAHITBGOU";
-		schlüssellänge(c, 3);
-		System.out.println("\n"+ vigenere(c, new int[]{-4,-7,-1}));
-	}
+public class App 
+{
+	public static boolean t = true;
+    public static void main( String[] args )
+    {
+        (new Thread(new Runnable(){
+        	public void run(){
+        		System.out.println("Hello world!");
+        	}
+        })).start();
+        System.out.println( "Hello World!" );
+        System.out.println( Vigenere( "ABCY", new int[]{2,3,4}));
+        System.out.println( Vigenere( "DADPSGSHCIITQLRGLDYEKCIYAPORLSKHFOETSLWLQBDAUPSNYYRIRNRCEK", new int[]{-2, -15, -3, -7, -0}));
+        System.out.println( Anzahl("Hello World!", 'o', 2, 3));
+        System.out.println( -3 % 5 );
+        String c = "HLNFYJXPTGOFRTBXOFQHUMRFVJIEYMIZCEICENFKLMEUHIYTXTBPZEMLFRAAMMGIYVRNFMUFVCJKLOIYFGOJJMSIKJIZFIUUHLDOBOKDVVKFNLESJIHHNESTRPDLAPIMGIUUPPDLIFOHORAHITBGOU";
+        Test(c, 4);
+        System.out.println( Vigenere( c, new int[]{-4, -7, -1}));
+        System.out.println( Vigenere( "ILFLNI", new int[]{-4, -24, -18, -3, -9, 5}));
 
-	public static int stelleimalphabet(char x) {
-
-		return x - 'A' + 1;
-	}
-
-	public static char shift(char x, int k) {
-		if (k < 0)
-			k += 26;
-		return (char) (((stelleimalphabet(x) + k - 1) % 26) + 'A');
-	}
-
-	public static String vigenere(String X, int[] k) {
-		String Y = "";
-		for (int i = 0; i < X.length(); i++) {
-			Y += shift(X.charAt(i), k[i % k.length]);
-
-		}
-		return Y;
-	}
-
-	public static int anzahl(String X, char x, int a, int s) {
-		int res = 0;
-		for (int i = a - 1; i < X.length(); i += s) {
-			if (X.charAt(i) == x)
-				res++;
-		}
-		return res;
-	}
-
-	public static char alledurchgehenBUCHSTABE(String X, int a, int s) {
-		int temp = 0;
-		int maximum = 0;
-		char buchstabe = '_';
-		for (char c = 'A'; c <= 'Z'; c++) {
-			temp = anzahl(X, c, a, s);
-			if (temp > maximum){
-				maximum = temp;
-				buchstabe = c;}
-
-		}
-		System.out.print("Max: "+ maximum);
-		return buchstabe;
-
-	}
-
-	public static void schlüssellänge(String X, int smax){
-		for(int s=1; s <= smax; s++){
-		System.out.print("\nSchlüssellänge: " + s + " ");
-		 for(int a = 1; a <= s; a ++ ){
-			System.out.print(" (" + a +  " ," + alledurchgehenBUCHSTABE(X, a, s) + ") ");
-		 }
-	}
-	}
+        
+    }
+    public static int StelleImAlphabet(char x)
+    {
+    	return x - 'A' + 1;
+    }
+    public static char Shift(char x, int k)
+    {
+    	if(k < 0)
+    		k += 26;
+    	return (char)((StelleImAlphabet(x) + k - 1) % 26 + 'A');
+    }
+    public static String Vigenere(String X, int[] k)
+    {
+    	String Y = "";
+    	for(int i = 0; i < X.length(); i++)
+    	{
+    		Y += Shift(X.charAt(i), k[i % k.length]);
+    	}
+    	return Y;
+    }
+    public static int Anzahl(String X, char x, int a, int s)
+    {
+    	int res = 0;
+    	for(int i = a - 1; i < X.length(); i += s)
+    	{
+    		if(X.charAt(i) == x)
+    			res++;
+    	}
+    	return res;
+    }
+    public static char AlleBuchstaben(String X, int a, int s)
+    {
+    	char buchstabe = '_';
+    	int max = 0;
+    	int temp = 0;
+    	for(char c = 'A'; c <= 'Z'; c++)
+    	{
+    		temp = Anzahl(X, c, a, s);
+    		if(temp > max)
+    		{
+    			max = temp;
+    			buchstabe = c;
+    		}
+    	}
+    	System.out.print("Max: " + max);
+    	return buchstabe;
+    }
+    public static void Test(String X, int smax)
+    {
+    	for(int s = 1; s <= smax; s++)
+    	{
+    		System.out.print("\nSchlüssellänge: " + s + " ");
+    		for(int a = 1; a <= s; a++)
+    		{
+    			System.out.print("(" + a + ", " + AlleBuchstaben(X, a, s) + "), ");
+    			//System.out.println("Buchstabe: " + AlleBuchstaben(X, a, s));
+    		}
+    	}
+    	System.out.println();
+    }
+    
 }
--- testaid2/src/test/java/testgid2/testaid2/AppTest.java
+++ testaid2/src/test/java/testgid2/testaid2/AppTest.java
@@ -33,8 +33,8 @@ public class AppTest
      */
     public void testApp()
     {
-    	assertTrue( App.stelleimalphabet('Z') == 26 );
-    	assertTrue( App.shift('A', 2)== 'D') ;
+    	assertTrue( App.StelleImAlphabet('Z') == 26);
+    	assertTrue( App.Shift('Y', 2) == 'A');
         assertTrue( true );
     }
 }
